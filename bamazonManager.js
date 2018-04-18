21780a63a562546c1b5b2427973f5a4fdca7154d
Kyle Weingart <kyle.weingart@gmail.com>
2018-04-18 12:59:03 -0400

    updates to bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -9,7 +9,7 @@ var inquirer = require("inquirer");
 
 // var itemIDArray = [];
 // var chosenItem;
-// var chosenQuantity;
+var chosenQuantity;
 // var stockQuantity;
 // var totalPrice;
 
@@ -38,53 +38,111 @@ connection.connect(function(err) {
 // ==========================================================================================
 
 function displayMenuOptions() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View Products for Sale",
-          "View Low Inventory",
-          "Add to Inventory",
-          "Add New Product"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
         case "View Products for Sale":
           productsView();
           break;
-  
+
         case "View Low Inventory":
           lowInventoryView();
           break;
-  
+
         case "Add to Inventory":
           addInventory();
           break;
-  
+
         case "Add New Product":
           addProduct();
           break;
-        }
-      });
-  }
-
- function productsView() {
-      console.log("productsView function");
-  }
-  
-  function lowInventoryView() {
-    console.log("lowinventoryView function");
+      }
+    });
 }
 
+function productsView() {
+  //   console.log("productsView function");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(
+      "Product Inventory:" +
+        "\n" +
+        "======================================="
+    );
+    for (var i = 0; i < res.length; i++) {
+      // console.log(res);
+      //   itemIDArray.push(res[i].item_id);
+      //   console.log(itemIDArray);
+      console.log(
+        "Item ID: " +
+          res[i].item_id +
+          "\n" +
+          "Product Name: " +
+          res[i].product_name +
+          "\n" +
+          "Price: $" +
+          res[i].price +
+          "\n" +
+          "Quantity: $" +
+          res[i].stock_quantity +
+          "\n" +
+          "==============================="
+      );
+    }
+  });
+}
+
+function lowInventoryView() {
+//   console.log("lowinventoryView function");
+inquirer
+    .prompt(
+      {
+        name: "productNumber",
+        type: "input",
+        message: "What number would you like to use to check inventory quantities?"
+      }
+    )
+
+    .then(function(answer) {
+      
+          chosenQuantity = parseInt(answer.productNumber);
+          
+      })
+
+  connection.query(
+    "SELECT * FROM PRODUCTS WHERE stock_quantity < chosenQuantity",
+    // how do i do this with less than?
+    // {
+    //   stock_quantity: < chosenQuantity
+    // },
+    function(err, res) {
+      if (err) throw err;
+      stockQuantity = res[0].stock_quantity;
+      if (res[0].stock_quantity < chosenQuantity) {
+        console.log(
+          "Insufficient Quantity! There are only " +
+            res[0].stock_quantity +
+            " available. Please reenter the quantity you want to buy."
+        );
+    }
+})
+
+
 function addInventory() {
-    console.log("addInventory function");
+  console.log("addInventory function");
 }
 
 function addProduct() {
-    console.log("addProduct function");
+  console.log("addProduct function");
 }
-
-
